4d11c4b3e5fc922fedcbd3288adc0afcf9916aa7
alber <alber@DESKTOP-OM9CVC8>
2018-02-27 10:30:03 +0100

    examen_perella_repujat

--- examen_perella/src/examen_perella/examen_perella.java
+++ examen_perella/src/examen_perella/examen_perella.java
@@ -5,15 +5,12 @@ public class examen_perella {
 	public static void main (String[]args) {
 		printOne();
 		printOne();
-		printTwo();
+		
 	}
 	
 	public static void printOne() {
 		System.out.println("Hello World");
 	}
 	
-	public static void printTwo() {
-		printOne();
-		printOne();
-	}
+	
 } 
